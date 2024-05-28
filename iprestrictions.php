2ededecec4406e1a4251d549bdaeded7592ebb51
Jitendra Purohit <purohitjitend@gmail.com>
2024-05-28 15:11:36 +0530

    civix upgrade

--- iprestrictions.php
+++ iprestrictions.php
@@ -13,15 +13,6 @@ function iprestrictions_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function iprestrictions_civicrm_xmlMenu(&$files) {
-  _iprestrictions_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -41,107 +32,12 @@ function iprestrictions_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function iprestrictions_civicrm_postInstall() {
-  _iprestrictions_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function iprestrictions_civicrm_uninstall() {
-  _iprestrictions_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function iprestrictions_civicrm_enable() {
   _iprestrictions_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function iprestrictions_civicrm_disable() {
-  _iprestrictions_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function iprestrictions_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _iprestrictions_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function iprestrictions_civicrm_managed(&$entities) {
-  _iprestrictions_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function iprestrictions_civicrm_caseTypes(&$caseTypes) {
-  _iprestrictions_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function iprestrictions_civicrm_angularModules(&$angularModules) {
-  _iprestrictions_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function iprestrictions_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _iprestrictions_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_entityTypes
- */
-function iprestrictions_civicrm_entityTypes(&$entityTypes) {
-  _iprestrictions_civix_civicrm_entityTypes($entityTypes);
 }
 
 function iprestrictions_civicrm_cron($jobManager) {
@@ -225,9 +121,8 @@ function iprestrictions_civicrm_validateForm($formName, &$fields, &$files, &$for
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
  *
-function iprestrictions_civicrm_preProcess($formName, &$form) {
 
-} // */
+ // */
 
 /**
  * Implements hook_civicrm_navigationMenu().
